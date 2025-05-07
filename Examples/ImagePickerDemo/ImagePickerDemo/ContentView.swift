b2ab0ce4fafcb5f7e0c516b09f33d95ce358ae30
Evan Deaubl <evan@deaubl.name>
2025-05-07 06:09:44 -0700

    Create demo app project

---
+++ Examples/ImagePickerDemo/ImagePickerDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImagePickerDemo
+//
+//  Created by Evan Deaubl on 5/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Examples/ImagePickerDemo/ImagePickerDemo/ImagePickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePickerDemoApp.swift
+//  ImagePickerDemo
+//
+//  Created by Evan Deaubl on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ImagePickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
